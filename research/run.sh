3d5c76bc4642dec0cca42c1c3637adc02e9c0c16
luoweile <luoweile2008@126.com>
2018-01-13 10:13:20 +0800

    run on tinymind

--- research/run.sh
+++ research/run.sh
@@ -9,8 +9,12 @@ echo current dir is $PWD
 export PYTHONPATH=$PYTHONPATH:$DIR:$DIR/slim:$DIR/object_detection
 
 # 定义各目录
-output_dir=/home/taylor/Documents/homework/week08/quiz-w8-data/output  # 训练目录
-dataset_dir=/home/taylor/Documents/homework/week08/quiz-w8-data # 数据集目录，这里是写死的，记得修改
+#output_dir=/home/taylor/Documents/homework/week08/quiz-w8-data/output  # 训练目录
+#dataset_dir=/home/taylor/Documents/homework/week08/quiz-w8-data # 数据集目录，这里是写死的，记得修改
+
+# tinymind path
+output_dir=/data/luoweile/my-objectdetection/output  # 训练目录
+dataset_dir=/data/luoweile/my-objectdetection # 数据集目录，这里是写死的，记得修改
 
 train_dir=$output_dir/train
 checkpoint_dir=$train_dir
